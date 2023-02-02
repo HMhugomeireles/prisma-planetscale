aae42fe1906da84c92ac07d1452c2db54406f5dd
Hugo Meireles <meireles.hmm@gmail.com>
2023-02-02 09:21:04 +0000

    connecting with plantscale with prisma

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+  await prisma.user.create({
+    data: {
+      name: 'test',
+      email: 'test@prisma.io',
+      posts: {
+        create: { title: 'Hello World test' },
+      },
+      profile: {
+        create: { bio: 'I like turtles test' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
